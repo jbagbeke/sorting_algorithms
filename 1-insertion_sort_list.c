af913a7bff2e439e818fa03c694f7f394cbe3525
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-09-25 17:28:21 +0000

    Insert_sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp1, *current = *list, *tmp2, *reversed;
 
-	if (!list)
+	if (!list || (*list)->next == NULL)
 		return;
 
 	while (current->next)
