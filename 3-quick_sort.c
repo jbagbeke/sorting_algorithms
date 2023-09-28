4eb639c2e876a57a9484965db3bf899e580018c9
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-27 17:16:12 -0700

    ALX_AND_ME

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,25 +13,31 @@ int partition_it(int *array, int low, int high, int size)
 	int tmp, i, pivot;
 
 	pivot = array[high];
-	printf("HELLO\n");
-	fflush(stdout);
-	for (i = low; i < high - 1; i++)
+
+	for (i = low; i < high; i++)
 	{
 		if (array[i] <= pivot)
 		{
-			tmp = array[i];
-			array[i] = array[low];
-			array[low] = tmp;
+			if (low < i)
+			{
+				tmp = array[i];
+				array[i] = array[low];
+				array[low] = tmp;
 
-			print_array(array, size);
+				print_array(array, size);
+			}
 			low++;
 		}
 	}
 
-	tmp = array[low];
-	array[low] = pivot;
-	array[high] = tmp;
-	printf("%d\n", low);
+	if (pivot < array[low])
+	{
+		tmp = array[low];
+		array[low] = pivot;
+		array[high] = tmp;
+		print_array(array, size);
+	}
+
 	return (low);
 }
 
@@ -52,7 +58,6 @@ void quick_partition(int *array, int low, int high, int size)
 	ppos = partition_it(array, low, high, size);
 
 	quick_partition(array, low, ppos - 1, size);
-	printf("HEYYYAA:: %d\n", ppos);
 	quick_partition(array, ppos + 1, high, size);
 	}
 }
@@ -67,20 +72,4 @@ void quick_partition(int *array, int low, int high, int size)
 void quick_sort(int *array, size_t size)
 {
 	quick_partition(array, 0, size - 1, size);
-}
-
-
-
-
-int main(void)
-{
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    quick_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
 }
